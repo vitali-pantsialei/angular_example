b04c3af45c89869bc442fc4674bb42e92277ff9f
Vitali Pantsialei <bitvitalby@gmail.com>
2015-03-13 15:20:50 +0300

    angular example

---
+++ myApp.js
@@ -0,0 +1,310 @@
+var data = [
+  {
+      "id": 1,
+      "name": "grit",
+      "full_name": "mojombo/grit",
+      "owner": {
+          "login": "mojombo",
+          "id": 1,
+          "avatar_url": "https://1.gravatar.com/avatar/25c7c18223fb42a4c6ae1c8db6f50f9b?d=https%3A%2F%2Fidenticons.github.com%2Fc4ca4238a0b923820dcc509a6f75849b.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/mojombo/grit",
+      "description": "Grit gives you object oriented read/write access to Git repositories via Ruby.",
+      "fork": false,
+      "url": "https://api.github.com/repos/mojombo/grit"
+  },
+  {
+      "id": 26,
+      "name": "merb-core",
+      "full_name": "wycats/merb-core",
+      "owner": {
+          "login": "wycats",
+          "id": 4,
+          "avatar_url": "https://0.gravatar.com/avatar/428167a3ec72235ba971162924492609?d=https%3A%2F%2Fidenticons.github.com%2Fa87ff679a2f3e71d9181a67b7542122c.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/wycats/merb-core",
+      "description": "Merb Core: All you need. None you don't.",
+      "fork": false,
+      "url": "https://api.github.com/repos/wycats/merb-core"
+  },
+  {
+      "id": 27,
+      "name": "rubinius",
+      "full_name": "rubinius/rubinius",
+      "owner": {
+          "login": "rubinius",
+          "id": 317747,
+          "avatar_url": "https://0.gravatar.com/avatar/8a664b7c5ca834af3e7e49d3a6160082?d=https%3A%2F%2Fidenticons.github.com%2F8a2a02b12e404a00b49c0154892fd9c0.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/rubinius/rubinius",
+      "description": "Rubinius, the Ruby Environment",
+      "fork": false,
+      "url": "https://api.github.com/repos/rubinius/rubinius"
+  },
+  {
+      "id": 28,
+      "name": "god",
+      "full_name": "mojombo/god",
+      "owner": {
+          "login": "mojombo",
+          "id": 1,
+          "avatar_url": "https://1.gravatar.com/avatar/25c7c18223fb42a4c6ae1c8db6f50f9b?d=https%3A%2F%2Fidenticons.github.com%2Fc4ca4238a0b923820dcc509a6f75849b.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/mojombo/god",
+      "description": "Ruby process monitor",
+      "fork": false,
+      "url": "https://api.github.com/repos/mojombo/god"
+  },
+  {
+      "id": 29,
+      "name": "jsawesome",
+      "full_name": "vanpelt/jsawesome",
+      "owner": {
+          "login": "vanpelt",
+          "id": 17,
+          "avatar_url": "https://2.gravatar.com/avatar/1da36d4c1f34454de6c07855098675f6?d=https%3A%2F%2Fidenticons.github.com%2F70efdf2ec9b086079795c442636b55fb.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/vanpelt/jsawesome",
+      "description": "Awesome JSON",
+      "fork": false,
+      "url": "https://api.github.com/repos/vanpelt/jsawesome"
+  },
+  {
+      "id": 31,
+      "name": "jspec",
+      "full_name": "wycats/jspec",
+      "owner": {
+          "login": "wycats",
+          "id": 4,
+          "avatar_url": "https://0.gravatar.com/avatar/428167a3ec72235ba971162924492609?d=https%3A%2F%2Fidenticons.github.com%2Fa87ff679a2f3e71d9181a67b7542122c.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/wycats/jspec",
+      "description": "A JavaScript BDD Testing Library",
+      "fork": false,
+      "url": "https://api.github.com/repos/wycats/jspec"
+  },
+  {
+      "id": 35,
+      "name": "exception_logger",
+      "full_name": "defunkt/exception_logger",
+      "owner": {
+          "login": "defunkt",
+          "id": 2,
+          "avatar_url": "https://2.gravatar.com/avatar/b8dbb1987e8e5318584865f880036796?d=https%3A%2F%2Fidenticons.github.com%2Fc81e728d9d4c2f636f067f89cc14862c.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/defunkt/exception_logger",
+      "description": "Unmaintained. Sorry.",
+      "fork": false,
+      "url": "https://api.github.com/repos/defunkt/exception_logger"
+  },
+  {
+      "id": 36,
+      "name": "ambition",
+      "full_name": "defunkt/ambition",
+      "owner": {
+          "login": "defunkt",
+          "id": 2,
+          "avatar_url": "https://2.gravatar.com/avatar/b8dbb1987e8e5318584865f880036796?d=https%3A%2F%2Fidenticons.github.com%2Fc81e728d9d4c2f636f067f89cc14862c.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/defunkt/ambition",
+      "description": "include Enumerable — Unmaintained",
+      "fork": false,
+      "url": "https://api.github.com/repos/defunkt/ambition"
+  },
+  {
+      "id": 42,
+      "name": "restful-authentication",
+      "full_name": "technoweenie/restful-authentication",
+      "owner": {
+          "login": "technoweenie",
+          "id": 21,
+          "avatar_url": "https://2.gravatar.com/avatar/821395fe70906c8290df7f18ac4ac6cf?d=https%3A%2F%2Fidenticons.github.com%2F3c59dc048e8850243be8079a5c74d079.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/technoweenie/restful-authentication",
+      "description": "Generates common user authentication code for Rails/Merb, with a full test/unit and rspec suite and optional Acts as State Machine support built-in.",
+      "fork": false,
+      "url": "https://api.github.com/repos/technoweenie/restful-authentication"
+  },
+  {
+      "id": 43,
+      "name": "attachment_fu",
+      "full_name": "technoweenie/attachment_fu",
+      "owner": {
+          "login": "technoweenie",
+          "id": 21,
+          "avatar_url": "https://2.gravatar.com/avatar/821395fe70906c8290df7f18ac4ac6cf?d=https%3A%2F%2Fidenticons.github.com%2F3c59dc048e8850243be8079a5c74d079.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/technoweenie/attachment_fu",
+      "description": "Treat an ActiveRecord model as a file attachment, storing its patch, size, content type, etc.",
+      "fork": false,
+      "url": "https://api.github.com/repos/technoweenie/attachment_fu"
+  },
+  {
+      "id": 47,
+      "name": "bong",
+      "full_name": "topfunky/bong",
+      "owner": {
+          "login": "topfunky",
+          "id": 26,
+          "avatar_url": "https://1.gravatar.com/avatar/a9d024f5032b8de04d7c74528beb77ab?d=https%3A%2F%2Fidenticons.github.com%2F4e732ced3463d06de0ca9a15b6153677.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/topfunky/bong",
+      "description": "A benchmarking helper for httperf.",
+      "fork": false,
+      "url": "https://api.github.com/repos/topfunky/bong"
+  },
+  {
+      "id": 48,
+      "name": "microsis",
+      "full_name": "Caged/microsis",
+      "owner": {
+          "login": "Caged",
+          "id": 25,
+          "avatar_url": "https://0.gravatar.com/avatar/97c3a8eea9b7eaa9e1e93ea3cd47399f?d=https%3A%2F%2Fidenticons.github.com%2F8e296a067a37563370ded05f5a3bf3ec.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/Caged/microsis",
+      "description": "SUPER OLD STUFF",
+      "fork": false,
+      "url": "https://api.github.com/repos/Caged/microsis"
+  },
+  {
+      "id": 52,
+      "name": "s3",
+      "full_name": "anotherjesse/s3",
+      "owner": {
+          "login": "anotherjesse",
+          "id": 27,
+          "avatar_url": "https://2.gravatar.com/avatar/50d10a8864accf0b2522c326381a4702?d=https%3A%2F%2Fidenticons.github.com%2F02e74f10e0327ad868d138f2b4fdd6f0.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/anotherjesse/s3",
+      "description": "psuedo s3 protocol for mozilla browsers",
+      "fork": false,
+      "url": "https://api.github.com/repos/anotherjesse/s3"
+  },
+  {
+      "id": 53,
+      "name": "taboo",
+      "full_name": "anotherjesse/taboo",
+      "owner": {
+          "login": "anotherjesse",
+          "id": 27,
+          "avatar_url": "https://2.gravatar.com/avatar/50d10a8864accf0b2522c326381a4702?d=https%3A%2F%2Fidenticons.github.com%2F02e74f10e0327ad868d138f2b4fdd6f0.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/anotherjesse/taboo",
+      "description": "The solution for tabitus of the browser ",
+      "fork": false,
+      "url": "https://api.github.com/repos/anotherjesse/taboo"
+  },
+  {
+      "id": 54,
+      "name": "foxtracs",
+      "full_name": "anotherjesse/foxtracs",
+      "owner": {
+          "login": "anotherjesse",
+          "id": 27,
+          "avatar_url": "https://2.gravatar.com/avatar/50d10a8864accf0b2522c326381a4702?d=https%3A%2F%2Fidenticons.github.com%2F02e74f10e0327ad868d138f2b4fdd6f0.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/anotherjesse/foxtracs",
+      "description": "firefox trac integration",
+      "fork": false,
+      "url": "https://api.github.com/repos/anotherjesse/foxtracs"
+  },
+  {
+      "id": 56,
+      "name": "fotomatic",
+      "full_name": "anotherjesse/fotomatic",
+      "owner": {
+          "login": "anotherjesse",
+          "id": 27,
+          "avatar_url": "https://2.gravatar.com/avatar/50d10a8864accf0b2522c326381a4702?d=https%3A%2F%2Fidenticons.github.com%2F02e74f10e0327ad868d138f2b4fdd6f0.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/anotherjesse/fotomatic",
+      "description": "Flash photo widget prototype - hacked at last SHDH of 2007",
+      "fork": false,
+      "url": "https://api.github.com/repos/anotherjesse/fotomatic"
+  },
+  {
+      "id": 61,
+      "name": "glowstick",
+      "full_name": "mojombo/glowstick",
+      "owner": {
+          "login": "mojombo",
+          "id": 1,
+          "avatar_url": "https://1.gravatar.com/avatar/25c7c18223fb42a4c6ae1c8db6f50f9b?d=https%3A%2F%2Fidenticons.github.com%2Fc4ca4238a0b923820dcc509a6f75849b.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/mojombo/glowstick",
+      "description": "A realtime, OpenGL graphing library for Ruby",
+      "fork": false,
+      "url": "https://api.github.com/repos/mojombo/glowstick"
+  },
+  {
+      "id": 63,
+      "name": "starling",
+      "full_name": "defunkt/starling",
+      "owner": {
+          "login": "defunkt",
+          "id": 2,
+          "avatar_url": "https://2.gravatar.com/avatar/b8dbb1987e8e5318584865f880036796?d=https%3A%2F%2Fidenticons.github.com%2Fc81e728d9d4c2f636f067f89cc14862c.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/defunkt/starling",
+      "description": "",
+      "fork": false,
+      "url": "https://api.github.com/repos/defunkt/starling"
+  },
+  {
+      "id": 65,
+      "name": "merb-more",
+      "full_name": "wycats/merb-more",
+      "owner": {
+          "login": "wycats",
+          "id": 4,
+          "avatar_url": "https://0.gravatar.com/avatar/428167a3ec72235ba971162924492609?d=https%3A%2F%2Fidenticons.github.com%2Fa87ff679a2f3e71d9181a67b7542122c.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/wycats/merb-more",
+      "description": "Merb More: The Full Stack. Take what you need; leave what you don't.",
+      "fork": false,
+      "url": "https://api.github.com/repos/wycats/merb-more"
+  },
+  {
+      "id": 68,
+      "name": "thin",
+      "full_name": "macournoyer/thin",
+      "owner": {
+          "login": "macournoyer",
+          "id": 22,
+          "avatar_url": "https://2.gravatar.com/avatar/0d949b795e64e062c4c001c6f5a6f3f3?d=https%3A%2F%2Fidenticons.github.com%2Fb6d767d2f8ed5d21a44b0e5886680cb9.png&r=x"
+      },
+      "private": false,
+      "html_url": "https://github.com/macournoyer/thin",
+      "description": "A very fast & simple Ruby web server",
+      "fork": false,
+      "url": "https://api.github.com/repos/macournoyer/thin"
+  }
+]
+
+
+angular.module('myApp', []).controller('myCtrl', function ($scope) {
+    $scope.repos = data;
+    $scope.query = '';
+    $scope.orderProp = false;
+    $scope.limit = 10;
+});
